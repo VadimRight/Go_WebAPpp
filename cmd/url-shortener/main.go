8e6ab63d0594c56197b954ab8d61abbcc7877f54
VadimRight <vadimright@mail.ru>
2024-03-26 19:43:29 +0300

    Fit: logger for different levels are done

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"log/slog"
 
 	"github.com/VadimRight/Go_WebApp/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
