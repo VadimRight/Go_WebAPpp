0efab1538d5ac19c2125b38c8c1b890f41b55675
VadimRight <vadimright@mail.ru>
2024-04-01 21:51:45 +0300

    Now in main routers declared

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log/slog"
-	"net/http"
 	"os"
 
 	"github.com/VadimRight/Go_WebApp/internal/config"
 	"github.com/VadimRight/Go_WebApp/internal/lib/logger/handlers/slogpretty"
-	"github.com/VadimRight/Go_WebApp/internal/server"
 	mwlogger "github.com/VadimRight/Go_WebApp/internal/server/middleware/logger"
 	"github.com/VadimRight/Go_WebApp/internal/storage/postgres"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
+
 
 const (
 	envLocal = "local"
@@ -23,6 +21,7 @@ const (
 )
 
 func main() {
+
 	cfg := config.MustLoad()
 	log := setupLogger(envLocal)
 	log.Info(
@@ -32,20 +31,19 @@ func main() {
 	)
 	log.Info(
 		"this is log",
-		slog.String("Postgres Name: ", cfg.Postgres_Name),
-		slog.String("Postgres Port: ", cfg.Postgres_Port),
-		slog.String("Postgres Host: ", cfg.Postgres_Host),
-		slog.String("Postgres User: ", cfg.Postgres_User),
-		slog.String("Server Port: ", cfg.Server_Port),
-		slog.Duration("Timeout: ", cfg.Timeout),
-		slog.Duration("Idle Timeout: ", cfg.IdleTimeout),
+		slog.String("Postgres Name", cfg.Postgres_Name),
+		slog.String("Postgres Port", cfg.Postgres_Port),
+		slog.String("Postgres Host", cfg.Postgres_Host),
+		slog.String("Postgres User", cfg.Postgres_User),
+		slog.String("Server Port", cfg.Server_Port),
+		slog.Duration("Timeout", cfg.Timeout),
+		slog.Duration("Idle Timeout", cfg.IdleTimeout),
 	)
 	db := postgres.InitDB()
 	fmt.Println(db)
 	test_add := postgres.TestAddUrl()
 	fmt.Println(test_add)
-
-	router := chi.NewRouter()
+		router := chi.NewRouter()
 	router.Use(middleware.RequestID)
 	router.Use(middleware.RealIP)
 	router.Use(middleware.Logger)
@@ -53,18 +51,10 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	http.HandleFunc("/", server.GetRoot)
-	http.HandleFunc("/hello", server.GetHello)
-	port := fmt.Sprintf(":%s", cfg.Server_Port)
-	err := http.ListenAndServe(port, nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
+
 
 }
+
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
